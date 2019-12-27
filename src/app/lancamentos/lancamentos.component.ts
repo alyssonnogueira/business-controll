79eae62e2e54709ccb4483d08b21dedd375ee591
Rafael Nogueira <homidastreva018@outlook.com>
2019-12-27 16:09:56 -0300

    Adicionei bota de add lancamentos, criei o modal

--- src/app/lancamentos/lancamentos.component.ts
+++ src/app/lancamentos/lancamentos.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit, ViewChild} from '@angular/core';
 import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
 import {Lancamento} from '../model/lancamento';
 import {LancamentoService} from '../services/lancamento.service';
+import { LancamentoModalComponent } from '../lancamento-modal/lancamento-modal.component';
+import {MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-lancamentos',
@@ -16,7 +18,7 @@ export class LancamentosComponent implements OnInit {
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
   @ViewChild(MatSort, {static: true}) sort: MatSort;
 
-  constructor(private  lancamentoService: LancamentoService ) {
+  constructor(private  lancamentoService: LancamentoService, public dialog: MatDialog ) {
     const lancamentos = lancamentoService.obterTodosLancamentos();
     this.dataSource = new MatTableDataSource(lancamentos);
   }
@@ -32,5 +34,16 @@ export class LancamentosComponent implements OnInit {
     if (this.dataSource.paginator) {
       this.dataSource.paginator.firstPage();
     }
+  } 
+ 
+  adicionarLancamento(){
+    const dialogRef = this.dialog.open(LancamentoModalComponent, {
+      width: '250px',
+      data: {}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      });
   }
 }
